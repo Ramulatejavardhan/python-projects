96814be5017e2d90bb6adba13d0502748cb28fee
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-05-18 19:43:35 +0530

    intial

---
+++ rock-paper-scissor.py
@@ -0,0 +1,9 @@
+choice=input("choice the option?:(r,p,s):").lower()
+if choice == "r":
+    print("u choose rock")
+elif choice == "p":
+    print("u choose paper")
+elif choice =="s":
+    print("u choose scissor")
+else:
+    print("invalid choice")
